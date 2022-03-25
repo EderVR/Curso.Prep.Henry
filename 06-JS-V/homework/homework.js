7794fb6c19a98f3e0b7720459a28f9a65259c657
Eder Vidal <edervidal1234@gmail.com>
2022-03-25 01:29:18 -0600

    Ejercicios JSV Completado

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -83,6 +83,12 @@ function agregarStringInvertida() {
       }
     }
 }
+//Persona.prototype.detalle = function(){
+  //return{
+    //Nombre : this.nombre
+    //Apellido: this.apellido,
+    //Edad: this.edad,
+    //Domicilio: this.domicilio,
 
 function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
